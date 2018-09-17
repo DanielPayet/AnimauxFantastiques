420507a90a3097ed85c6ac11fb5363a09c5a19b2
Daniel Payet <danieldu974@gmail.com>
2018-09-17 08:26:47 +0200

    first commit

---
+++ src/main/java/fr/wizard/animauxfantastiques/animauxfantastiques/AnimauxFantastiquesApplication.java
@@ -0,0 +1,12 @@
+package fr.wizard.animauxfantastiques.animauxfantastiques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimauxFantastiquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimauxFantastiquesApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/wizard/animauxfantastiques/animauxfantastiques/AnimauxFantastiquesApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.wizard.animauxfantastiques.animauxfantastiques;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnimauxFantastiquesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
