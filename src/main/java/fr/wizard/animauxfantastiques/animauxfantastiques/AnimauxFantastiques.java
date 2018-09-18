31f1286509c57f25a8121707307f5e733e505992
Daniel Payet <danieldu974@gmail.com>
2018-09-18 11:51:50 +0200

    Lecture du fichier de données

---
+++ src/main/java/fr/wizard/animauxfantastiques/animauxfantastiques/AnimauxFantastiques.java
@@ -0,0 +1,35 @@
+package fr.wizard.animauxfantastiques.animauxfantastiques;
+
+public class AnimauxFantastiques {
+    private String nom = "";
+    private String classification = "";
+    private String habitat = "";
+    private String premierApercu = "";
+    private String description = "";
+
+    AnimauxFantastiques() {
+    }
+
+    String getNom() {
+        return nom;
+    }
+
+    String getClassification() {
+        return classification;
+    }
+
+    String getHabitat() {
+        return habitat;
+    }
+
+    String getPremierApercu() {
+        return premierApercu;
+    }
+
+    String getDescription() {
+        return description;
+    }
+}
+
+
+
