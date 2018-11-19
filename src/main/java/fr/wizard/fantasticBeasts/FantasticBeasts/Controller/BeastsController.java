6e6414e15ca6bced0144e261df6bb8b15316f56d
Victor Jozwicki <victor.jozwicki@gmail.com>
2018-11-19 15:58:17 +0100

    Added filters for smart functions
    can access with http://localhost:8080/beasts?locationId=1&categoryId=3
    
    Need help for locationId

--- src/main/java/fr/wizard/fantasticBeasts/FantasticBeasts/Controller/BeastsController.java
+++ src/main/java/fr/wizard/fantasticBeasts/FantasticBeasts/Controller/BeastsController.java
@@ -8,13 +8,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class BeastsController {
 
     @GetMapping("/beasts")
-    public List<Beast> getBeasts() {
-        return DataBase.getBeasts();
+    public List<Beast> getBeasts(@RequestParam(value = "locationId", required = false) Integer locationId, @RequestParam(value = "categoryId", required = false) Integer categoryId) {
+        List<Beast> beasts = DataBase.getBeasts();
+        if(locationId != null) {
+            beasts = beasts.stream().filter(beast -> {
+                //int[] locationIds = beast.getLocationsId();
+                // foreach(int locaId : locationIds)
+                //{ return locaId == locationId }
+                return true;
+            }).collect(Collectors.toList());
+        }
+        if(categoryId != null) { // Test in case beasts is already emtpy ? What is it ?
+            beasts = beasts.stream().filter(beast -> beast.getClassificationId() == categoryId).collect(Collectors.toList());
+        }
+        return beasts;
     }
 
     @GetMapping("/beasts/{id}")
