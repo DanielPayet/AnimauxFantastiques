15e94acedbc97e46cb27513cfeabd4bc62fcd609
Daniel Payet <danieldu974@gmail.com>
2019-01-05 10:58:46 +0100

    fix launch

--- src/main/java/fr/wizard/fantasticBeasts/FantasticBeasts/FantasticBeastsApplication.java
+++ src/main/java/fr/wizard/fantasticBeasts/FantasticBeasts/FantasticBeastsApplication.java
@@ -1,27 +1,13 @@
 package fr.wizard.fantasticBeasts.FantasticBeasts;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import fr.wizard.fantasticBeasts.DataBase.DataBase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-@EnableAdminServer
 public class FantasticBeastsApplication {
 	public static void main(String[] args) {
 		DataBase.load();
 		SpringApplication.run(FantasticBeastsApplication.class, args);
-	}
-
-	@Configuration
-	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.authorizeRequests().anyRequest().permitAll()
-					.and().csrf().disable();
-		}
 	}
 }
