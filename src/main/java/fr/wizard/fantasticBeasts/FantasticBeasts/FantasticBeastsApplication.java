5435a81e73ce99dbf0038b25981bc29b300e2150
Victor Jozwicki <victor.jozwicki@gmail.com>
2019-01-06 22:38:26 +0100

    swagger integration
    Integrated swagger but UI doesn't work
    
    access by http://localhost:8080/v2/api-docs and http://localhost:8080/swagger-ui.html

--- src/main/java/fr/wizard/fantasticBeasts/FantasticBeasts/FantasticBeastsApplication.java
+++ src/main/java/fr/wizard/fantasticBeasts/FantasticBeasts/FantasticBeastsApplication.java
@@ -3,8 +3,10 @@ package fr.wizard.fantasticBeasts.FantasticBeasts;
 import fr.wizard.fantasticBeasts.DataBase.DataBase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FantasticBeastsApplication {
 	public static void main(String[] args) {
 		DataBase.load();
